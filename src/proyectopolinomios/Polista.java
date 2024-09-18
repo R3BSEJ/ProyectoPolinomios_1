d00fa916a39958e8cc8ce6939d4158970240984e
R3BSEJ <jesber201@gmail.com>
2024-09-17 23:00:21 -0500

    Cambios (10:59PM)
    Correciones y adicion de codigo

--- src/proyectopolinomios/Polista.java
+++ src/proyectopolinomios/Polista.java
@@ -2,87 +2,113 @@ package proyectopolinomios;
 
 import javax.swing.JOptionPane;
 
-public class Polista 
-{
+public class Polista {
+
     private Nodo cab;
 
     public Polista() {
-        cab=null;
+        cab = null;
     }
 
     public Nodo getCab() {
         return cab;
     }
-        
-     //Método para mostrar
-    public String mostrar() 
-    {
+
+    //Método para mostrar
+    public String mostrar() {
         String salida = "<html>";
-        Nodo p=cab;
-        while(p!=null)
-        {
-            if (p.getCoef() > 0 && p!=cab) 
-            {
+        Nodo p = cab;
+        while (p != null) {
+            if (p.getCoef() > 0 && p != cab) {
                 salida = salida + "+ ";
             }
-            salida=salida+p.getCoef();
-            if(p.getExp()==1)
-            {
-                salida=salida+"X";
+            salida = salida + p.getCoef();
+            if (p.getExp() == 1) {
+                salida = salida + "X";
             }
-            if(p.getExp()>1)
-            {
-                salida = salida +"x<sup>" + (int)p.getExp()+"</sup>";
+            if (p.getExp() > 1) {
+                salida = salida + "x<sup>" + (int) p.getExp() + "</sup>";
             }
-            p=p.getLiga();
-         }
+            p = p.getLiga();
+        }
         salida = salida + "</html>";
         return salida;
     }
-    
-     public void ingresarTerminos() {
+
+    public float evaluar(float x) {
+        float resultado = 0;
+        Nodo p = cab;
+        while (p != null) {
+            resultado += p.getCoef() * Math.pow(x, p.getExp());
+            p = p.getLiga();
+        }
+        return resultado;
+    }
+
+    public Polista sumar(Polista B) {
+        Polista C = new Polista();
+        Nodo p1 = this.cab;
+        Nodo p2 = B.getCab();
+        Nodo x, ant = null;
+
+        while (p1 != null || p2 != null) {
+            Nodo nuevoTermino = null;
+            if (p1 != null && (p2 == null || p1.getExp() > p2.getExp())) {
+                nuevoTermino = new Nodo(p1.getExp(), p1.getCoef());
+                p1 = p1.getLiga();
+            } else if (p2 != null && (p1 == null || p2.getExp() > p1.getExp())) {
+                nuevoTermino = new Nodo(p2.getExp(), p2.getCoef());
+                p2 = p2.getLiga();
+            } else if (p1 != null && p2 != null && p1.getExp() == p2.getExp()) {
+                nuevoTermino = new Nodo(p1.getExp(), p1.getCoef() + p2.getCoef());
+                p1 = p1.getLiga();
+                p2 = p2.getLiga();
+            }
+            if (nuevoTermino != null) {
+                if (C.getCab() == null) {
+                    C.cab = nuevoTermino;
+                    ant = C.getCab();
+                } else {
+                    ant.setLiga(nuevoTermino);
+                    ant = nuevoTermino;
+                }
+            }
+        }
+        return C;
+    }
+
+    public void ingresarTerminos() {
         int exp;
         float coef;
-        Nodo p,ant,x;
-        
+        Nodo p, ant, x;
+
         String respuesta;
         respuesta = JOptionPane.showInputDialog("Desea ingresar término? S/N");
-        while (respuesta.equalsIgnoreCase("S")) 
-        {
+        while (respuesta.equalsIgnoreCase("S")) {
             coef = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el coeficiente: "));
             exp = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el exponente: "));
-            if (exp >= 0) 
-            {
-               p=cab;
-               ant=null;
-               while(p!=null && p.getExp()>exp)
-               {
-                   ant=p;
-                   p=p.getLiga();
-               }
-               if(p!=null &&p.getExp()==exp)
-               {
-                  JOptionPane.showMessageDialog(null,"Ya existe un término con ese exponente"); 
-               }
-               else
-               {
-                   x=new Nodo(exp,coef);
-                   x.setLiga(p);
-                   if(p==cab)
-                   {
-                       cab=x;
-                   }
-                   else
-                   {
-                       ant.setLiga(x);
-                   }
+            if (exp >= 0) {
+                p = cab;
+                ant = null;
+                while (p != null && p.getExp() > exp) {
+                    ant = p;
+                    p = p.getLiga();
                 }
-            } 
-            else 
-            {
+                if (p != null && p.getExp() == exp) {
+                    JOptionPane.showMessageDialog(null, "Ya existe un término con ese exponente");
+                } else {
+                    x = new Nodo(exp, coef);
+                    x.setLiga(p);
+                    if (p == cab) {
+                        cab = x;
+                    } else {
+                        ant.setLiga(x);
+                    }
+                }
+            } else {
                 JOptionPane.showMessageDialog(null, "El exponente no es válido");
             }
-           respuesta = JOptionPane.showInputDialog("Desea ingresar término? S/N");
-         }
-       }
+            respuesta = JOptionPane.showInputDialog("Desea ingresar término? S/N");
+        }
+    }
 }
--- src/proyectopolinomios/ProyectoPolinomios.java
+++ src/proyectopolinomios/ProyectoPolinomios.java
@@ -124,7 +124,7 @@ public class ProyectoPolinomios
        B=new Polvf2(canterm) ; 
        B.ingresarTerminos(canterm);
        
-       String opcion, menu="♦♣POLINOMIOS EN VECTOR FORMA 1♣♦\n" +
+       String opcion, menu="♦♣POLINOMIOS EN VECTOR FORMA 2♣♦\n" +
                             "1-	Mostrar\n" +
                             "2-	Evaluar\n" +
                             "3-	Sumar\n" +
@@ -172,63 +172,63 @@ public class ProyectoPolinomios
          
         }while(!opcion.equals("0"));
      }
-    //Método para el menú de polinomios en vector forma 1
-    public static void menuPolista()
-    {
-       Polista A,B,C;
-       float x;
-       
-//       A=new Polista()) ; 
-//       A.ingresarTerminos();
-//       B=new Polista() ; 
-//       B.ingresarTerminos();
-      String opcion, menu="♦♣POLINOMIOS EN VECTOR FORMA 1♣♦\n" +
-                            "1-	Mostrar\n" +
-                            "2-	Evaluar\n" +
-                            "3-	Sumar\n" +
-                            "4-	Multiplicar\n" +
-                            "5-	Dividir\n" +
-                            "6-	Comparar\n" +
-                            "7- Ir al menú principal\n"+
-                            "0-	Salir\n" +
-                            "Digite la opción";
-        
-        do
-        {
-            opcion=JOptionPane.showInputDialog(null,menu);
-            switch(opcion)
-            {
-//                case "1":JOptionPane.showMessageDialog(null,"Datos del Polinomio 1\n"+A.mostrar()
-//                                                        +"\nDatos del Polinomio 2\n"+B.mostrar());
-//                                                    
-//                break;
-//                case "2":x=Float.parseFloat(JOptionPane.showInputDialog("Ingrese el valor de X"));
-//                      A.evaluar(x);
-//                break;
-//                case "3": C=A.sumar(B);
-//                          JOptionPane.showMessageDialog(null,"Datos del Polinomio 1\n"+A.mostrar()
-//                                                        +"\nDatos del Polinomio 2\n"+B.mostrar()
-//                                                    +"\nDatos del Polinomio suma\n"+C.mostrar());
-//                break;
-//                case "4": C=A.multiplicar(B);
-//                          JOptionPane.showMessageDialog(null,"Datos del Polinomio 1\n"+A.mostrar()
-//                                                        +"\nDatos del Polinomio 2\n"+B.mostrar()
-//                                                    +"\nDatos del Polinomio suma\n"+C.mostrar());
-////                break;
-//               case "5": C=A.dividir(B);
-//                          JOptionPane.showMessageDialog(null,"Datos del Polinomio 1\n"+A.mostrar()
-//                                                        +"\nDatos del Polinomio 2\n"+B.mostrar()
-//                                                    +"\nDatos del Polinomio suma\n"+C.mostrar());
-//                break;
-//                case "6": A.comparar(B);
-//                          
-//                break;
-                case "7":menuPrincipal();
+   
+public static void menuPolista() {
+    Polista A = new Polista();
+    Polista B = new Polista();
+    Polista C;
+    float x;
+
+    String opcion, menu = "♦♣POLINOMIOS EN VECTOR FORMA 3(Lista)♣♦\n" +
+                          "1- Mostrar\n" +
+                          "2- Evaluar\n" +
+                          "3- Sumar\n" +
+                          "4- Multiplicar\n" +
+                          "5- Dividir\n" +
+                          "6- Comparar\n" +
+                          "7- Ir al menú principal\n" +
+                          "0- Salir\n" +
+                          "Digite la opción";
+
+    do {
+        opcion = JOptionPane.showInputDialog(null, menu);
+        switch (opcion) {
+            case "1":
+                JOptionPane.showMessageDialog(null, "Datos del Polinomio 1\n" + A.mostrar()
+                        + "\nDatos del Polinomio 2\n" + B.mostrar());
                 break;
-                case "0":System.exit(0);
-            }
-         
-        }while(!opcion.equals("0"));
-     }
+            case "2":
+                x = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el valor de X"));
+                JOptionPane.showMessageDialog(null, "Evaluación de A: " + A.evaluar(x)
+                        + "\nEvaluación de B: " + B.evaluar(x));
+                break;
+            case "3":
+                C = A.sumar(B);
+                JOptionPane.showMessageDialog(null, "Datos del Polinomio suma\n" + C.mostrar());
+                break;
+            case "4":
+                C = A.multiplicar(B);
+                JOptionPane.showMessageDialog(null, "Datos del Polinomio multiplicado\n" + C.mostrar());
+                break;
+            case "5":
+                C = A.dividir(B);
+                JOptionPane.showMessageDialog(null, "Datos del Polinomio división\n" + C.mostrar());
+                break;
+            case "6":
+                boolean iguales = A.comparar(B);
+                JOptionPane.showMessageDialog(null, "¿Son iguales? " + (iguales ? "Sí" : "No"));
+                break;
+            case "7":
+                menuPrincipal();
+                break;
+            case "0":
+                System.exit(0);
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Opción no válida");
+        }
+
+    } while (!opcion.equals("0"));
+}
     
 }
